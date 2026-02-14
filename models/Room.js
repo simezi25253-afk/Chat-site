1c52d373ec2974a893648b44d93034ae9b6df765
simezi25253-afk <simezi25253@gmail.com>
2026-02-15 00:10:50 +0900

    Update Room.js

--- models/Room.js
+++ models/Room.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
+const messageSchema = new mongoose.Schema({
+  id: String,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // ← ObjectIdに変更
+  nickname: String,
+  text: String,
+  ts: Number,
+  readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+});
+
 const roomSchema = new mongoose.Schema({
   name: { type: String, required: true },
   password: { type: String },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  leader: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  messages: [messageSchema]
 });
 
 module.exports = mongoose.model('Room', roomSchema);

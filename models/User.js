f43fd49ee98235bc9d1826adc44903c88b2cf4d7
simezi25253-afk <simezi25253@gmail.com>
2026-02-14 20:39:47 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  accessCode: { type: String, required: true, unique: true } // ← 追加！
 });
 
 userSchema.pre('save', async function (next) {
